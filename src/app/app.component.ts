baa588b42827ec9dc7bfbbe0075d347d40968085
ksprasad001 <mail2k.sriprasad@gmail.com>
2017-07-03 13:26:34 -0500

    changed url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import {DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
 export class AppComponent implements OnInit {
   first_url = 'https://www.youtube.com/embed/STzX5I9KHDE';
   second_url = 'https://www.youtube.com/embed/2f5KNdFnvoA';
-  third_url = 'https://www.youtube.com/embed/MOPJiAPNaI';
+  third_url = 'https://www.youtube.com/embed/MOPJiAPNaIg';
   fourth_url= 'https://www.youtube.com/embed/dRXtnD9dLzo';
   fifth_url= 'https://www.youtube.com/embed/F3iRZipAqw8';
   current_url: SafeResourceUrl;
