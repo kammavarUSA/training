60024dffba171a32486c4654a1b585012f564764
ksprasad001 <mail2k.sriprasad@gmail.com>
2017-07-03 13:16:08 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  first_url = 'https://www.youtube.com/embed/STzX5I9KHDE';
+  second_url = 'https://www.youtube.com/embed/2f5KNdFnvoA';
+  third_url = 'https://www.youtube.com/embed/MOPJiAPNaI';
+  fourth_url= 'https://www.youtube.com/embed/dRXtnD9dLzo';
+  fifth_url= 'https://www.youtube.com/embed/F3iRZipAqw8';
+  current_url: SafeResourceUrl;
+  constructor(private sanitizer: DomSanitizer) {
+  }
+
+  updateSrc(url) {
+    this.current_url = this.sanitizer.bypassSecurityTrustResourceUrl(url)
+  }
+  ngOnInit() {
+    this.current_url = this.sanitizer.bypassSecurityTrustResourceUrl(this.first_url)
+  }
 }
+
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Angular2FontawesomeModule } from 'angular2-fontawesome/angular2-fontawesome'
 
-import { AppComponent } from './app.component';
+
+import {AppComponent} from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    Angular2FontawesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
